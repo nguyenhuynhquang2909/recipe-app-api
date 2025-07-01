3444df0b9522fd2b451ceea602efefe725e9f7a4
quang.nguyen <quang.nguyen@nng.bz>
2025-07-01 14:30:41 +0700

    feat: add swagger ui config

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework',
+    'drf_spectacular',
+    'rest_framework.authtoken',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +135,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
